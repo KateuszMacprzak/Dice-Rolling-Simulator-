65f875323afb187fc490d6d3baf0432890a9ff54
Mateusz@MSI <matkac98@gmail.com>
2020-12-09 23:19:33 +0100

    Pierwsze prawidłowe działanie
    App działa poprawnie, dodanie warunków i funkcji losującej

--- gitkostka.py
+++ gitkostka.py
@@ -4,3 +4,42 @@ print('Welcome in dice rolling simulator %s'%name)
 x='y'
 while (x=='y'):
     a=random.randint(1,6)
+    if (a==1):
+        print('___')
+        print('   ')
+        print(' 0 ')
+        print('   ')
+        print('___')
+    elif (a==2):
+        print('___')
+        print(' 0 ')
+        print(' 0 ')
+        print('   ')
+        print('___')
+    elif (a==3):
+        print('___')
+        print(' 0 ')
+        print(' 0 ')
+        print(' 0 ')
+        print('___')
+    elif (a==4):
+        print('___')
+        print('0 0')
+        print('   ')
+        print('0 0')
+        print('___')
+    elif (a==5):
+        print('___')
+        print('0 0')
+        print(' 0 ')
+        print('0 0')
+        print('___')
+    else:
+        print('___')
+        print('000')
+        print('000')
+        print('000')
+        print('___')
+    x=input('Enter y if you want to roll once again or n if you want to exit...')
+    if (x=='n'):
+        print('Thanks for your time, have a nice day %s'%name)
